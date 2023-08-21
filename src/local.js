05c97db48bb15f26a9e57088905071dd0bb52339
Davis Gaunt <bdg0015@uah.edu>
2023-08-21 15:35:21 -0500

    testing hash blocking

--- src/local.js
+++ src/local.js
@@ -38,7 +38,7 @@ class LocalUpload{
             )
             await hashChunk(chunk);
         }
-        const hash = this.hasher.digest('binary').toString('base64');
+        const hash = this.hasher.digest('hex');
         return Promise.resolve(hash);
     };
 
