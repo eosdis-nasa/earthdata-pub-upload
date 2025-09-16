0e858daba8747dc169bd96f003b6fab17a95bc00
camposeddie <eddie.campos@uah.edu>
2025-09-16 13:49:12 -0400

    logging

--- src/cue-utility.js
+++ src/cue-utility.js
@@ -140,10 +140,10 @@ class CueFileUtility{
             return ({error: "Failed to get upload URL"});
         }
         try{
-            console.log({presignedUrl: presignedUrlResponse.presigned_url, fileObj, fileType, fileSize: fileObj.size});
             const uploadResult = await this.signedPost(presignedUrlResponse.presigned_url, fileObj, fileType, fileObj.size, onProgress);
             etag = uploadResult.headers.get('ETag');
         }catch(err){
+            console.log(err);
             return ({error: "Failed to upload to bucket"});
         }
         try{
